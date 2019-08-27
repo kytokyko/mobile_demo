867401155bf7c26dae984db920e60b2fbf2ba99c
swaminav <swaminav@MAA-5CG7041Q2D.ant.amazon.com>
2019-08-27 18:54:38 +0530

    new commit

--- src/main/java/com/leafBot/pages/DatePickerPage.java
+++ src/main/java/com/leafBot/pages/DatePickerPage.java
@@ -1,5 +1,7 @@
 package com.leafBot.pages;
 
+import java.util.List;
+
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
@@ -15,10 +17,19 @@ public class DatePickerPage extends Annotations {
 	}
 	
 	@FindBy(how=How.XPATH, using="//i[@class='glyphicon glyphicon-th']") WebElement calender;
+	@FindBy(how=How.XPATH, using="//table[@class='table-condensed']//td") List<WebElement> web_data_table;
+
 
 	public DatePickerPage select_date()
 	{
 		click(calender);
+		int size = web_data_table.size();
+		
+		for(int i=0;i<size;i++)
+		{
+			System.out.println(web_data_table.get(i).getText());
+			
+		}
 		return this;
 	}
 
